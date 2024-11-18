91ae186a6df21caa46f31fdbb9ebc4130cb2c6a3
Shalatiey23 <vchauke357@gmail.com>
2024-11-18 20:17:38 +0200

    changed few lines

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const pool = require("./config/db");
 
 import { Application } from "express";
 
 const app: Application = express();
 app.use(bodyParser.json());
+
+pool.connect((err: Error, client: any, release: any) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    process.exit(1);
+  } else {
+    console.log("Connected to the PostgreSQL database.");
+    release();
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
